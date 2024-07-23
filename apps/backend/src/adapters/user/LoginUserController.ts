3c500a1bd311723f4ae5c286630df0edfb33862c
marlissonsilva <marlyssonsilva005@gmail.com>
2024-07-23 08:19:15 -0300

    Retorna nome e email do usuário

--- apps/backend/src/adapters/user/LoginUserController.ts
+++ apps/backend/src/adapters/user/LoginUserController.ts
@@ -1,30 +1,30 @@
-import { UsuarioFacade } from 'adapters'
-import { CryptoProvider, RepositoryUser } from 'core'
-import { Express } from 'express'
-import Erros from '../../utils/Erros'
-import ProvedorJWT from '../../external/auth/ProvedorJWT'
+import {UsuarioFacade} from "adapters";
+import {CryptoProvider, RepositoryUser} from "core";
+import {Express} from "express";
+import Erros from "../../utils/Erros";
+import ProvedorJWT from "../../external/auth/ProvedorJWT";
 
 export default class LoginUsuarioController {
-    constructor(
-        readonly servidor: Express,
-        readonly repo: RepositoryUser,
-        readonly provedorCripto: CryptoProvider,
-        readonly provedorJWT: ProvedorJWT
-    ) {
-        servidor.post('/user/login', async (req, res) => {
-            try {
-                const { email, password } = req.body
-                const facade = new UsuarioFacade(repo, provedorCripto)
-                const usuario = await facade.login({ email, password })
-                const token = provedorJWT.gerar({
-                    id: usuario.id,
-                    nome: usuario.name,
-                    email: usuario.email,
-                })
-                res.status(200).json({ token })
-            } catch (e: any) {
-                res.status(400).send(Erros.tratar(e))
-            }
-        })
-    }
+  constructor(
+    readonly servidor: Express,
+    readonly repo: RepositoryUser,
+    readonly provedorCripto: CryptoProvider,
+    readonly provedorJWT: ProvedorJWT
+  ) {
+    servidor.post("/user/login", async (req, res) => {
+      try {
+        const {email, password} = req.body;
+        const facade = new UsuarioFacade(repo, provedorCripto);
+        const usuario = await facade.login({email, password});
+        const token = provedorJWT.gerar({
+          id: usuario.id,
+          nome: usuario.name,
+          email: usuario.email,
+        });
+        res.status(200).json({token, nome: usuario.name, email: usuario.email});
+      } catch (e: any) {
+        res.status(400).send(Erros.tratar(e));
+      }
+    });
+  }
 }
